e89bc92068270ac560d2784c1f75abedc271f36b
g-i-t-h-u-b-u-s-e-r-n-a-m-e <m@crowcastle.net>
2026-02-14 19:09:51 -0500

    Add files via upload

--- run.js
+++ run.js
@@ -1,14 +1,18 @@
 var timmy = 420000;
+var paused = false;
+
 openPuzzle();
 
 async function openPuzzle() {
-  chrome.windows.create(
-    {
-      url: "https://lichess.org/training",
-      focused: true,
-      incognito: false,
-    }
-  );
+  if (!document.getElementById("pause").checked) {
+    chrome.windows.create(
+      {
+        url: "https://lichess.org/training",
+        focused: true,
+        incognito: false,
+      }
+    );
+  }
   timmy = parseInt(document.getElementById("time").value, 10);
   setTimeout(() => {
         openPuzzle();
