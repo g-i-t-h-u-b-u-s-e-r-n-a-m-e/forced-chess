3e7b7d57388976b7732d8455c9f4e5a4372377a8
g-i-t-h-u-b-u-s-e-r-n-a-m-e <m@crowcastle.net>
2026-03-08 13:47:46 -0400

    Add files via upload

--- run.js
+++ run.js
@@ -1,13 +1,30 @@
 var timmy = 420000;
-var paused = false;
 
 openPuzzle();
 
 async function openPuzzle() {
-  if (!document.getElementById("pause").checked) {
+  if (!document.getElementById("pause").checked && document.getElementById("site").value == 1) {
     chrome.windows.create(
       {
         url: "https://lichess.org/training",
+        focused: true,
+        incognito: false,
+      }
+    );
+  }
+  else if (!document.getElementById("pause").checked && document.getElementById("site").value == 2) {
+    chrome.windows.create(
+      {
+        url: "https://www.chess.com/puzzles",
+        focused: true,
+        incognito: false,
+      }
+    );
+  }
+  else if (!document.getElementById("pause").checked && document.getElementById("site").value == 3) {
+    chrome.windows.create(
+      {
+        url: "https://woogles.io/puzzle",
         focused: true,
         incognito: false,
       }
