0d0abb90d23127eccf26be4b4448a441fa4d5202
g-i-t-h-u-b-u-s-e-r-n-a-m-e <m@crowcastle.net>
2026-03-09 17:08:05 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,15 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const startButton = document.getElementById("start-button");
-
-  startButton.addEventListener("click", function () {
-    init();
-  });
-});
-function init() {
   chrome.tabs.create(
     {
       url: "/background.html",
       active: true
     }
   );
-}
+});
